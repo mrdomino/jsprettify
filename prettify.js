517a9ee8d1da570ed0c66b4885e45cd42cd8a14a
Steve Dee <steve@smartercode.net>
2010-02-23 19:09:46 -0500

    More tests passing.

--- prettify.js
+++ prettify.js
@@ -36,8 +36,10 @@ prettify.prettify = function(text) {
     ['---', prettify.entities.emdash],
     ['--', prettify.entities.endash],
     ['\\.\\.\\.', prettify.entities.hellip],
-    ["'", prettify.entities.lsquo],
-    ['"', prettify.entities.ldquo]
+    ["(^| )'", '$1' + prettify.entities.lsquo],
+    ['(^| )"', '$1' + prettify.entities.ldquo],
+    ["'$", prettify.entities.rsquo],
+    ['"$', prettify.entities.rdquo]
   ];
   goog.array.forEach(subs, function(arr) {
     var re = new RegExp(arr[0], 'g');
