1c3a84c2d3a1a7ce893668e2aabf17ad1659b45b
Steve Dee <steve@smartercode.net>
2010-03-03 18:01:24 -0500

    This seems to work.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-// javascript:document.write(%27<script%20type=%22text/javascript%22%20src=%22%27%20+%27http://jsprettify.googlecode.com/hg/prettify.js%22></%27%20+%20%27script>%27);var%20tags%20=%20document.getElementsByTagName(%27textarea%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}tags%20=%20document.getElementsByTagName(%27input%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{if%20(tags[i].type%20==%20%27text%27)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}}
+// javascript:var%20script%20=%20document.createElement(%27script%27);script.src%20=%20%27http://jsprettify.googlecode.com/hg/prettify.js%27;script.type%20=%20%27text/javascript%27;document.body.appendChild(script);function%20prettifyMe()%20{var%20tags%20=%20document.getElementsByTagName(%27textarea%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}tags%20=%20document.getElementsByTagName(%27input%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{if%20(tags[i].type%20==%20%27text%27)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}}}setTimeout(prettifyMe,%20500);
 // Copyright 2010 Steven Dee. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,15 +13,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-document.write('<script type="text/javascript" src="' +
-    'http://jsprettify.googlecode.com/hg/prettify.js"></' + 'script>');
-var tags = document.getElementsByTagName('textarea');
-for (var i = 0; i < tags.length; i++) {
-  tags[i].value = jsprettify.prettifyStr(tags[i].value);
-}
-tags = document.getElementsByTagName('input');
-for (var i = 0; i < tags.length; i++) {
-  if (tags[i].type == 'text') {
+var script = document.createElement('script');
+script.src = 'http://jsprettify.googlecode.com/hg/prettify.js';
+script.type = 'text/javascript';
+document.body.appendChild(script);
+function prettifyMe() {
+  var tags = document.getElementsByTagName('textarea');
+  for (var i = 0; i < tags.length; i++) {
     tags[i].value = jsprettify.prettifyStr(tags[i].value);
   }
+  tags = document.getElementsByTagName('input');
+  for (var i = 0; i < tags.length; i++) {
+    if (tags[i].type == 'text') {
+      tags[i].value = jsprettify.prettifyStr(tags[i].value);
+    }
+  }
 }
+setTimeout(prettifyMe, 500);
