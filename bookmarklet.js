284bc4d031af9eadb42389a416078ee0c32206c4
Steve Dee <steve@smartercode.net>
2010-03-03 17:50:23 -0500

    Effects of running make_bookmarklet.pl on bookmarklet.js.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
+// javascript:document.write(%27<script%20type=%22text/javascript%22%20src=%22%27%20+%27http://jsprettify.googlecode.com/hg/prettify.js%22></%27%20+%20%27script>%27);var%20tags%20=%20document.getElementsByTagName(%27textarea%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}tags%20=%20document.getElementsByTagName(%27input%27);for%20(var%20i%20=%200;%20i%20<%20tags.length;%20i++)%20{if%20(tags[i].type%20==%20%27text%27)%20{tags[i].value%20=%20jsprettify.prettifyStr(tags[i].value);}}
 // Copyright 2010 Steven Dee. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
