734ee15547f06bf1dfda79854a18f3da298d02aa
Steve Dee <steve@smartercode.net>
2010-03-03 17:30:13 -0500

    Simple build script.

---
+++ build.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Copyright 2010 Steven Dee. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import sep
+from subprocess import call
+
+compiler_jar = sep.join(['..', 'closure-compiler', 'compiler.jar'])
+closure_dir = sep.join(['..', 'closure-library', 'closure'])
+calcdeps = sep.join([closure_dir, 'bin', 'calcdeps.py'])
+output_file = 'prettify-comp.js'
+input_file = 'prettify.js'
+
+call([calcdeps, '-o', 'compiled', '--compiler_jar=' + compiler_jar, '-p',
+    closure_dir, '-p', '.', '--output_file=' + output_file, '-i', input_file,
+    '-f', '--compilation_level', '-f', 'ADVANCED_OPTIMIZATIONS'])
