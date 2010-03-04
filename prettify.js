39561484a8996690a23b01524f386383ce3a41bd
Steve Dee <steve@smartercode.net>
2010-03-04 01:52:27 -0500

    Now we prettify nodes that are not direct descendants of body.

--- prettify.js
+++ prettify.js
@@ -117,6 +117,7 @@ window['prettify'] = function() {
   var es = document.getElementsByClassName('prettify');
   var opts = {'uglyTags': ['code', 'pre'], 'uglyClass': 'keepugly'};
   for (var i = 0; i < es.length; i++) {
-    document.body.replaceChild(jsprettify.prettifyHtml(es[i], opts), es[i]);
+    var parentNode = es[i].parentNode;
+    parentNode.replaceChild(jsprettify.prettifyHtml(es[i], opts), es[i]);
   }
 };
