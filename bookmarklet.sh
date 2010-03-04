514586871afe830f2df77dd0f72c7c87170cb6cd
Steve Dee <steve@smartercode.net>
2010-03-04 02:23:05 -0500

    bookmarklet.sh puts generated file in the right place.

--- bookmarklet.sh
+++ bookmarklet.sh
@@ -4,7 +4,7 @@ perl make_bookmarklet.pl bookmarklet-comp.js > bookmarklet-tmp.js 2>/dev/null
 
 bm=`head -n1 bookmarklet-tmp.js | cut -c4-`
 
-cat <<EOF >bookmarklet.html
+cat <<EOF >gadgets/bookmarklet.html
 <!doctype html>
 <title>Prettify Bookmarklet</title>
 <p><a href="${bm}" title="jsprettify bookmarklet">Prettify Inputs</a>
