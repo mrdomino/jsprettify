21f9ff0a8038a20327950011528b9cdd41c6c2f3
Steve Dee <steve@smartercode.net>
2010-03-04 06:14:55 -0500

    Added tag bookmarklet-1 for changeset 938361d8745f

---
+++ bookmarklet.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+[ -f bookmarklet-comp.js ] || python build.py
+perl make_bookmarklet.pl bookmarklet-comp.js > bookmarklet-tmp.js 2>/dev/null
+
+bm=`head -n1 bookmarklet-tmp.js | cut -c4-`
+
+cat <<EOF >gadgets/bookmarklet.html
+<!doctype html>
+<title>Prettify Bookmarklet</title>
+<p><a href="${bm}" title="jsprettify bookmarklet">Prettify Inputs</a>
+EOF
+
+rm bookmarklet-tmp.js
