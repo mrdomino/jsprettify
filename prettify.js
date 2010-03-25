a4f7f68aff2d935bd65ec4b13a324c90ab1c7303
Steve Dee <steve@smartercode.net>
2010-03-25 18:11:50 -0400

    Export jsprettify from prettify.js

--- prettify.js
+++ prettify.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+goog.provide('jsprettify');
 goog.provide('jsprettify.entities');
 goog.provide('jsprettify.prettifyHtml');
 goog.provide('jsprettify.prettifyStr');
