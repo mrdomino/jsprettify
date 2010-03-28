b8514f958410efbf17a701fe23c929e5890f559e
Steve Dee <steve@smartercode.net>
2010-03-28 02:41:36 -0400

    New bookmarklet

--- bookmarklet.sh
+++ bookmarklet.sh
@@ -7,8 +7,9 @@ bm=`head -n1 bookmarklet-tmp.js | cut -c4-`
 
 cat <<EOF >gadgets/bookmarklet.html
 <!doctype html>
+<style type="text/css">body,html{padding:0;margin:0;}</style>
 <title>Prettify Bookmarklet</title>
-<p><a href="${bm}" title="jsprettify bookmarklet">Prettify Inputs</a>
+<div><a href="${bm}"><img src="../media/jsprettify-bookmarklet.png" alt="jsPrettify"></a></div>
 EOF
 
 rm bookmarklet-tmp.js
