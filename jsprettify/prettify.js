d90d1a08961ad5999fee62a101a1208f7e93f310
Steve Dee <steve@smartercode.net>
2010-04-06 02:37:08 -0400

    Factor prettify.js into different files

---
+++ jsprettify/prettify.js
@@ -0,0 +1,32 @@
+// Copyright 2010 Steven Dee. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+goog.provide('jsprettify.prettify');
+
+goog.require('goog.dom');
+goog.require('jsprettify.prettifyHtml');
+
+
+/**
+ * Auto-prettifies everything with classname 'prettify' in a document. This
+ * can be used in e.g. a window.onload function to automatically prettify all
+ * text when the window has loaded.
+ */
+jsprettify.prettify = function() {
+  var es = goog.dom.$$(null, 'prettify');
+  var opts = {'uglyTags': ['code', 'pre', 'script'], 'uglyClass': 'keepugly'};
+  for (var i = 0; i < es.length; i++) {
+    goog.dom.replaceNode(jsprettify.prettifyHtml(es[i], opts), es[i]);
+  }
+};
---
+++ jsprettify/prettify_html.js
@@ -0,0 +1,53 @@
+// Copyright 2010 Steven Dee. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+goog.provide('jsprettify.prettifyHtml');
+
+goog.require('goog.array');
+goog.require('goog.dom');
+goog.require('jsprettify.prettifyStr');
+
+/**
+ * Prettifies HTML Nodes by recursively prettifying their child text nodes.
+ * This function operates nondestructively -- a prettified HTML fragment is
+ * returned, and can replace the existing one to prettify a document.
+ * @param {Node|null} e Node to start prettifying.
+ * @param {{uglyTags: Array.<string>, uglyClass: string}} opt_args Optional
+ *     arguments to customize the behavior of the function. 'uglyTags' is an
+ *     array of tagnames not to prettify. 'uglyClass' is a string consisting
+ *     of the class not to prettify.
+ * @return {Node|null} Prettified version of the passed node.
+ */
+jsprettify.prettifyHtml = function(e, opt_args) {
+  var args = opt_args || {};
+  var uglyTags = args['uglyTags'] || [];
+  var uglyClass = args['uglyClass'] || '';
+  if (e == null) {
+    return null;
+  }
+  var ret = e.cloneNode(true);
+  if (e.nodeType == goog.dom.NodeType.TEXT) {
+    goog.dom.setTextContent(ret,
+        jsprettify.prettifyStr(goog.dom.getTextContent(ret)));
+  } else if (! goog.array.contains(uglyTags, e.nodeName.toLowerCase()) &&
+      ! (e.className && e.className == uglyClass)) {
+    var curChildren = ret.childNodes;
+    for (var i = 0; i < curChildren.length; i++) {
+      goog.dom.replaceNode(jsprettify.prettifyHtml(curChildren[i], opt_args),
+          curChildren[i]);
+    }
+  }
+  return ret;
+};
+
